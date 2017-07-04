f170857c2fe6627e4e35382a25f6915dcca4447f
vikas <vikasrajuinversity@gmail.com>
2017-07-04 09:15:28 -0700

    main file is created

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-# main file
-from self_info  import self_info
-from get_own_post import get_own_post
+#main file.
+from self_info import self_info
+from get_own_post import  get_own_post
+
 #fetching own information
 #self_info()
+
 #fetching our own recent data
 get_own_post()
-
