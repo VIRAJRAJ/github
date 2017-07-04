ea5fc8a9de0a7b4a7b8835a90d167e89fb189e6f
vikas <vikasrajuinversity@gmail.com>
2017-07-03 19:08:21 -0700

    git_user_id() returnsuser_id

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-def self_info():
-    # main file
+# main file
+from self_info  import self_info
+from get_own_post import get_own_post
+#fetching own information
+#self_info()
+#fetching our own recent data
+get_own_post()
+
