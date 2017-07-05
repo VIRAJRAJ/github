8ad5c5f15bcf402b5af8eb055d57be34c6b1cf82
vikas <vikasrajuinversity@gmail.com>
2017-07-05 09:28:08 -0700

    get_own_post.py function created

--- get_own_post.py
+++ get_own_post.py
@@ -4,14 +4,14 @@ from constants import BASE_URL, APP_ACCESS_TOKEN
 
 def get_own_post():
     #function login
-    request_url = (BASE_URL, + 'users/self/media/recent/?access_token=%s')%(APP_ACCESS_TOKEN)
+    request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % (APP_ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
     own_media = requests.get(request_url).json()
 
     if own_media['meta']['code'] == 200:
         # extract post ID
         if len(own_media['data']):
-            image_name = own_media['data']['0']['id']+'.jpg'
+            image_name = own_media['data']['0']['id']+'.jpeg'
             image_url = own_media['data']['0']['images']['standard_resolutation']['url']
             urllib.urlretrive(image_url, image_name)
             print 'your image has been downloded'
