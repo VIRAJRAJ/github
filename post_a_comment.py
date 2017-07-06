afc99aa9974e600885fea1d16a7fcc5c14ecac13
vikas <vikasrajuinversity@gmail.com>
2017-07-06 16:06:16 -0700

    post_a_commit file has been created

---
+++ post_a_comment.py
@@ -0,0 +1,21 @@
+#Function declaration to make a comment on the recent post of the user
+import requests
+from constants import APP_ACCESS_TOKEN, BASE_URL
+from get_user_post import get_user_post
+from get_post_id import get_post_id
+insta_username = "jyotithakur15111"
+def post_a_comment(insta_username):
+    media_id = get_post_id(insta_username)
+
+    comment_text = raw_input("Your comment: ")
+    payload = {"access_token": APP_ACCESS_TOKEN, "text" : comment_text}
+    request_url = (BASE_URL + 'media/%s/comments') % (media_id)
+    print 'POST request url : %s' % (request_url)
+
+    make_comment = requests.post(request_url, payload).json()
+
+    if make_comment['meta']['code'] == 200:
+        print "Successfully added a new comment!"
+    else:
+        print "Unable to add comment. Try again!"
+post_a_comment(insta_username="jyotithakur15111")
